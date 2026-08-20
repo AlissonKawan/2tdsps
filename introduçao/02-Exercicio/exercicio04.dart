9d75c7bc496c79a9941c6511d065fc16663d0d80
AlissonKawan <rm567598@fiap.com.br>
2026-08-19 22:12:33 -0300

    adicionando ex

---
+++ introduçao/02-Exercicio/exercicio04.dart
@@ -0,0 +1,9 @@
+void main() {
+
+  String nome = "Alisson" ;
+  int idade = 19 ;
+  double altura = 1.76 ;
+  String local = "São Paulo";
+
+  print("Meu nome é ${nome} tenho ${idade} anos, ${altura}m de altura e moro em ${local}");
+}
