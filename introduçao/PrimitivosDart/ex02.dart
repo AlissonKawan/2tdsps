2a83cf466851cc3fd9b7ffb4c68d3f1b4c901316
AlissonKawan <rm567598@fiap.com.br>
2026-08-26 22:31:57 -0300

    adicionando mais um ex

---
+++ introduçao/PrimitivosDart/ex02.dart
@@ -0,0 +1,8 @@
+void main(){
+  final nomeAluno = 'Alisson';
+  const nomeCurso = 'Dart para Flutter';
+  const cargaHoraria = 120;
+
+
+  print('Nome aluno: $nomeAluno, Nome do curso: $nomeCurso, Carga horaria: $cargaHoraria');
+}
